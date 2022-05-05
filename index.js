e05408c9c2e5f2b793d4b50febbb707cf629342a
VolodumurGruv <gru.v@i.ua>
2022-05-05 11:23:48 +0300

    slight chanches

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const limit = parseInt(gets(5));
 
-function gets(val) {
-	return val;
-}
-
 for (let i = 1; i <= limit; i++) {
 	const x = i ** 2;
 	const y = i ** 3;
 
 	console.log(i, x, y);
+}
+
+function gets(val) {
+	return val;
 }
